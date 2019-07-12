131c725c64df35976ac41372c747f6714e2c7e66
Aikawa Kazuhiko <javamessage@hotmail.com>
2019-07-12 14:29:15 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IG+YXYoYXHnnyNt5Ugfp7c7ME85Cndf63UaJkZICfoa6bDZpEooYURSFFCU7PV+ALGK1yAUxiW+vFrHZJFNIyEmHsWLrx60Y6XMf4R7pr6+mfhGdbxO8xlpxGjPz0uGKb+J4akxmJQCIL0GikF5i6AdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = '2i2YN6Dvw0Ks/3fff8s3SxvtY8RPON8B5YihWrvQ7NsP+SEoK57SyELWSjtmQpTo5VJ/itHQci2Zi5riMypY/YVLEo10/DlB1jaNQpHSgKIZQ1s8QOaDGYOaVmmmazHDRsXzDfa/6jSDkm4prHgmrQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
